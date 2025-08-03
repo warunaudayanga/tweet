33568d28a9369438b8ddcc51cb0309f4b888316a
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2025-08-03 23:53:23 +0530

    lint

--- apps/tweet-gateway/src/main.ts
+++ apps/tweet-gateway/src/main.ts
@@ -1,19 +1,23 @@
+// noinspection JSIgnoredPromiseFromCall
+
 /**
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app/app.module";
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    const app = await NestFactory.create(AppModule);
+    const globalPrefix = "api";
+    app.setGlobalPrefix(globalPrefix);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
